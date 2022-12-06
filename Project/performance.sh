5854caaeb1727950094aaad534502c813826eb35
S-furi <stefano.furi@studio.unibo.it>
2022-12-06 17:25:47 +0100

    Changed CPU grepping

--- Project/performance.sh
+++ Project/performance.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-n_cores=$(lscpu | grep "Core(s) per socket"  | grep -Eo "[0-9]+");
+n_cores=$(lscpu | grep -m 1 "CPU(s)" | grep -Eo "[0-9]+");
 
 for(( i=$n_cores; $i > 0 ; i=$i-1 )); do
   elapsed=$(OMP_NUM_THREADS=${i} ./omp-sph 10000 | grep "Elapsed" | cut -d "=" -f 2)
